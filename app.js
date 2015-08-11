dc40fc3aeaa9bf1a4e49f2c21451a7da4df3b8da
Karen Mao <karen.mao707@gmail.com>
2015-08-11 19:21:45 -0400

    completed mongoose install

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -37,6 +39,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+  swig.setDefaults({ cache: false });
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
