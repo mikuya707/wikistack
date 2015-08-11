73afb46d0440779ab6e8a548a2466f23e2ae07b5
Cristina <cristina.colon+git@gmail.com>
2015-08-11 19:43:22 -0400

    models

--- models/index.js
+++ models/index.js
@@ -4,3 +4,30 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/wikistack');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+var statuses = ['open','closed'];
+
+var pageSchema = new mongoose.Schema({
+  title:    {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content:  {type: String, required: true},
+  status:   {type: String, enum: statuses},
+  date:     {type: Date, default: Date.now},
+  author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+var userSchema = new mongoose.Schema({
+  name: {
+  	first: {type: String, required: true}, 
+  	last: {type: String, required: true}
+  },
+  email: {type: String, required: true, unique: true}
+});
+
+var Page = mongoose.model('Page', pageSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  Page: Page,
+  User: User
+};
