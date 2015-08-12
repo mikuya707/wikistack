091c26dfef31a4bdc6591671021b38bdf3a1e0ff
Karen Mao <karen.mao707@gmail.com>
2015-08-12 15:36:32 -0400

    creating and routing user in progress

--- routes/wiki.js
+++ routes/wiki.js
@@ -23,15 +23,32 @@ router
 		});
 	})
 	.post('/', function(req, res, next){
-		var page = new Page({
-			title: req.body.title,
-			content: req.body.content
-		})
-		//console.log(page.urlTitle);
-		page.save().then(function(page){
-			//console.log(page);
-			res.redirect(page.toObject({virtuals:true}).route);
+		var email = req.body.authorEmail;
+		User.findOne().select("email").exec(function(err, user){
+			if(!user){
+				var user = new User({first: req.body.authorName.split(" ")[0], 
+									last: req.body.authorName.split(" ")[1],
+									email: req.body.authorEmail})
+				user.save().then(function(user){
+					var page = new Page({
+						title: req.body.title,
+						content: req.body.content,
+						author: user._id
+					})
+					page.save().then(function(page){
+						res.redirect(page.toObject({virtuals: true}).route)
+					})
+			   })
+			}		
+			
 		}).then(null,next);
+		
+		//console.log(page.urlTitle);
+
+		// page.save().then(function(page){
+		// 	//console.log(page);
+		// 	res.redirect(page.toObject({virtuals:true}).route);
+		// }).then(null,next);
 		
 		//res.send(); //some sort page object
 	})
